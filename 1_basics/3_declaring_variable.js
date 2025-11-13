b66bf51b0e817749d5214c979f72920cad80baf0
Yura389 <165561350+Yura389@users.noreply.github.com>
2025-11-13 09:31:21 +0000

    Add commit

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,42 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let 
+ * 3) const
+ */
+
+var name = '홍길동';
+console.log(name);
+
+var age = 28;
+console.log(age);
+
+let bts = '방탄소년단';
+console.log(bts);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변결할 수 있다.
+ */
+
+bts = '박지민';
+console.log(bts);
+
+const Bts = '방탄소년단';
+console.log(Bts);
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ */
+
+var name = '프론트엔드';
+console.log(name);
+
+let girlfriend;
+console.log(girlfriend);
+
+
